403aa044ac7dfa1e1d30d660819aac515b24120e
karim <karim1430@gmail.com>
2024-10-10 16:19:27 +0300

    first commit

---
+++ lib/views/responsive_dashbord_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashbord/widgets/adaptive_layout.dart';
+import 'package:responsive_dashbord/widgets/layout_desktop.dart';
+import 'package:responsive_dashbord/widgets/responsive_font_size.dart';
+
+class ResponsiveDashbordView extends StatelessWidget {
+  const ResponsiveDashbordView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AdaptiveLayout(
+          layoutMobile: (context) => SizedBox(),
+          layoutTablet: (context) => SizedBox(),
+          layoutDesktop: (context) => LayoutDesktop()),
+    );
+  }
+}
